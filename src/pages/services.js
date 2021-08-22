770fa2593e24748c0b9419287f6405c02b988032
Patrick Whitehouse <patrick1996@gmail.com>
2021-08-22 19:35:40 +0100

    validate html

--- src/pages/services.js
+++ src/pages/services.js
@@ -14,7 +14,8 @@ const Services = () => {
                 <p>We realise that taking care of your customers, employees and day to day business is already challenging. Especially in today’s volatile world. Striving to improve waste management and waste disposal options may not be a priority. This is where AD Supplies can be a valuable partner. We can help you to find the best waste management solution. We can upcycle your waste.</p>
                 <p>
                     Some of the ways we can help.
-                    <ul>
+                </p>
+                <ul>
                         <li>Bespoke waste storage and waste removal solutions</li>
                         <li>Liquid waste collections and semi dry waste collections</li>
                         <li>Bulk marine cargo salvage and removal</li>
@@ -23,7 +24,6 @@ const Services = () => {
                         <li>Catering waste collections</li>
                         <li>IBC, Dolav, Skip storage and collection</li>
                     </ul>
-                </p>
                 <p>We have an agile team happy to talk through your individual circumstances or make a visit to your site. We can assess the best on-site waste handling and waste haulage options. Our goal will be to remove the need to send your waste to landfill or trade effluent. By avoiding landfill or trade effluent, you could improve your businesses green credentials, and improve your credibility with your customers & stakeholders. You will also reduce cost.</p>
             </main>
         </Layout>
